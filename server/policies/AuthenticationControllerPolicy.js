f5031ba310049df3bc2f011645ed141aba95e7e9
Unknown <realague@gmail.com>
2018-12-16 14:51:48 +0100

    [+] Authentication

--- server/policies/AuthenticationControllerPolicy.js
+++ server/policies/AuthenticationControllerPolicy.js
@@ -2,13 +2,21 @@ const Joi = require('joi');
 
 module.exports = {
     register (req, res, next) {
+        const whiteList = "epitech.eu"
+
         const schema = {
+            email: Joi.string().regex(new RegExp('^.+\\..+@'+ whiteList + '$')),
             password: Joi.string().regex(new RegExp('^[a-zA-Z0-9]{8,32}$'))
         };
 
         const {error, value} = Joi.validate(req.body, schema);
         if (error) {
             switch (error.details[0].context.key) {
+                case 'email':
+                    res.status(400).send({
+                        error: 'You must register with a valid email.'
+                    });
+                    break;
                 case 'password':
                     res.status(400).send({
                         error: 'The password provided failed to match the following rules:' +
